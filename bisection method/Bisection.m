0665e00f0c62d245b0dcc98022182b2785468b72
Ahmed Khaled <ahmedkhaledabab@gmail.com>
2019-10-19 13:44:41 +0200

    added illustrative comment

--- bisection method/Bisection.m
+++ bisection method/Bisection.m
@@ -1,7 +1,8 @@
 function retval = Bisection (funcStr)
+  # converting the input to a real function
   funcStr = strcat("@(x)",funcStr);
   fh = str2func(funcStr);
-  y = fh(3);
-  disp(y);
-  retval = funcStr;
+  
+  
+  
 endfunction
