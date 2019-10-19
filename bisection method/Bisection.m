de856775e3c104c056471cf86011c51a9054e4ff
Ahmed Khaled <ahmedkhaledabab@gmail.com>
2019-10-19 13:40:22 +0200

    created Bisection function prototype and got input function from the user

---
+++ bisection method/Bisection.m
@@ -0,0 +1,7 @@
+function retval = Bisection (funcStr)
+  funcStr = strcat("@(x)",funcStr);
+  fh = str2func(funcStr);
+  y = fh(3);
+  disp(y);
+  retval = funcStr;
+endfunction
---
+++ bisection method/BisectionCaller.m
@@ -0,0 +1,2 @@
+str = input("Enter a function in x: ", 's');
+ans = Bisection(str)
