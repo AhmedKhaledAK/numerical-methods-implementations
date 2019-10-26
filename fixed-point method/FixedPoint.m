3115b3bd52c71af271eb04acb7a1d174303323c1
Ahmed Khaled <ahmedkhaledabab@gmail.com>
2019-10-26 22:34:47 +0200

    edited prototype of FixedPoint function and converted string to function

--- fixed-point method/FixedPoint.m
+++ fixed-point method/FixedPoint.m
@@ -1,3 +1,9 @@
-function retval = FixedPoint()
+function retval = FixedPoint(funcStr,x0, imax, es)
+  
+  # converting the input to a real function
+  funcStr = strcat("@(x)",funcStr);
+  gh = str2func(funcStr);
+  # end of converting
+  
   
 endfunction
